abfe936c7c5be8e63aa1ecb74de53e8fd05bfd3b
itunuogunfuye-cmd <255497632+itunuogunfuye-cmd@users.noreply.github.com>
2026-09-15 22:38:49 +0200

    Add Nouri route

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import Root from '@/components/Root'
 import Home from '@/pages/Home'
 import FocusDock from '@/pages/FocusDock'
 import LegoPoliceStory from '@/pages/LegoPoliceStory'
+import Nouri from '@/pages/Nouri'
 
 export const router = createBrowserRouter([
   {
@@ -12,6 +13,7 @@ export const router = createBrowserRouter([
       { index: true, Component: Home },
       { path: 'projects/focus-dock', Component: FocusDock },
       { path: 'projects/lego-police-story', Component: LegoPoliceStory },
+      { path: 'projects/nouri', Component: Nouri },
     ],
   },
 ])
